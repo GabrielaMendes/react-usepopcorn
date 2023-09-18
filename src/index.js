218214e5e481e329bedfea9b2ac897b710aea23b
Gabriela <gdemelo.mendes@gmail.com>
2023-09-18 16:46:09 -0300

    Created reusable star rating component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
-import StarRating from './components/StarRating';
+import StarRating from "./components/StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <StarRating />
-  </React.StrictMode>
+	<React.StrictMode>
+		{/* <App /> */}
+		<StarRating messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+		<StarRating size={24} color="green" defaultRating={3} />
+	</React.StrictMode>
 );
